4db6b5e4af6739cc78ff852769ba87d57f79e15a
Sophia Gold <sophiagoldnyc@gmail.com>
2018-04-05 03:48:53 -0400

    s/`= PowS a`/`= PowS [a]`

--- src/PowerSerious.hs
+++ src/PowerSerious.hs
@@ -1,18 +1,19 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE OverlappingInstances       #-}
 
 module PowerSerious where
 
 default (Integer,Rational,Double)
 infixr 9 #
 
-newtype PowS a = PowS a deriving Eq
--- newtype PowS a = PowS { fromPowS :: a } deriving Eq
+newtype PowS a = PowS [a] deriving Eq
+-- newtype PowS a = PowS { fromPowS :: [a] } deriving Eq
 
-instance (Num a, Eq a) => Num (PowS [a]) where
-   fromInteger c = PowS [fromInteger c]
-
+instance (Num a, Eq a) => Num (PowS a) where
+   fromInteger c = fromInteger c
+   
    negate (PowS fs) = PowS (map negate fs)
 
    PowS (f:ft) + PowS (g:gt) = PowS (f+g:t) where PowS t = PowS ft + PowS gt
@@ -24,8 +25,8 @@ instance (Num a, Eq a) => Num (PowS [a]) where
    PowS (f:ft) * PowS gs@(g:gt) = PowS (f*g:t) where PowS t =  PowS ft * PowS gs + PowS [f] * PowS gt
    _ * _ = PowS []
 
-instance (Fractional a, Eq a) => Fractional (PowS [a]) where
-   fromRational c = PowS [fromRational c]
+instance (Fractional a, Eq a) => Fractional (PowS a) where
+   fromRational c = fromRational c
 
    PowS (0:ft) / PowS gs@(0:gt) = PowS ft / PowS gt
    PowS (0:ft) / gs = PowS (0:t) where PowS t = PowS ft / gs
@@ -34,31 +35,31 @@ instance (Fractional a, Eq a) => Fractional (PowS [a]) where
    PowS [] / PowS (g:gt) = PowS []
    _ / _ = error "improper power series division"
 
-(#) :: (Eq a, Num a, Fractional a, Num (PowS [a]), Fractional (PowS [a])) => [a] -> [a] -> [a]
-(f:ft) # gs@(0:gt) = f : gt * ft # gs
-(f:ft) # gs@(g:gt) = [f] + gs * ft # gs -- ft must be polynomial
-[] # _ = []
-(f:_) # [] = [f]
+(#) :: (Eq a, Num a) => PowS a -> PowS a -> PowS a
+PowS (f:ft) # gs@(PowS (0:gt)) = PowS (f : gt) * PowS ft # gs
+PowS (f:ft) # gs@(PowS (g:gt)) = PowS [f] + gs * PowS ft # gs -- ft must be polynomial
+PowS [] # _ = PowS []
+PowS (f:_) # PowS [] = PowS [f]
 
-revert :: (Eq a, Num a, Fractional a, Num (PowS [a]), Fractional (PowS [a])) => [a] -> [a]
-revert (_:0:_) = error "revert f where f'(0)==0" 
-revert (0:ft) = rs where rs = 0 : 1 / ft # rs
-revert [f,f'] = [-f/f',1/f']
+revert :: (Eq a, Num a, Fractional a) => PowS a -> PowS a
+revert (PowS (_:0:_)) = error "revert f where f'(0)==0"
+revert (PowS (0:ft)) = rs where rs = PowS ([0, 1]) / PowS ft # rs
+revert (PowS [f,f']) = PowS [-f/f',1/f']
 revert _ = error "revert f where f(0)/=0"
 
-int :: (Num a, Fractional a, Num (PowS [a]), Fractional (PowS [a])) => [a] -> [a]
-int fs = 0 : zipWith (/) fs (map fromInteger [1..])
+int :: (Fractional a, Num (PowS a), Fractional (PowS a)) => PowS a -> PowS a
+int (PowS fs) = PowS (0 : zipWith (/) fs (map fromInteger [1..]))
+diff :: (Num a, Num (PowS a), Fractional (PowS a)) => PowS a -> PowS a
+diff (PowS (_:ft)) = PowS $ zipWith (*) ft (map fromInteger [1..])
 
-diff :: (Num a, Fractional a, Num (PowS [a]), Fractional (PowS [a])) => [a] -> [a]
-diff (_:ft) = zipWith (*) ft (map fromInteger [1..])
+tans :: (Eq a, Fractional a, Num (PowS a), Fractional (PowS a)) => PowS a
+tans = revert $ int 1 / PowS [1,0,1]
 
-tans = revert(int(1/(1:0:1)))
-
-sins :: (Num a, Fractional a, Num (PowS [a]), Fractional (PowS [a])) => [a]
+sins :: (Fractional a, Num (PowS a), Fractional (PowS a)) => PowS a
 sins = int coss
 
-coss :: (Num a, Fractional a, Num (PowS [a]), Fractional (PowS [a])) => [a]
+coss :: (Fractional a, Num (PowS a), Fractional (PowS a)) => PowS a
 coss = 1 - int sins
 
-pascal :: (Num a, Fractional a, Num (PowS [a]), Fractional (PowS [a])) => [[a]]
-pascal = 1/[1, -[1,1]]
+pascal :: (Eq a, Fractional a, Num (PowS a), Fractional (PowS a)) => PowS (PowS a)
+pascal = 1 / PowS [1, - PowS [1,1]]
