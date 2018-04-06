24e20935e9fb0e3aff7a7d2b7120d1e3bc5af827
Sophia Gold <sophiagoldnyc@gmail.com>
2018-04-05 22:05:02 -0400

    patched bug in `fromInteger`

--- src/PowerSerious.hs
+++ src/PowerSerious.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MonoLocalBinds             #-}
 {-# LANGUAGE RankNTypes                 #-}
 {-# OPTIONS_GHC -Wno-missing-methods    #-}
 
@@ -27,28 +28,28 @@ instance Show a => Show (PowS' a) where
   show (PowS' ps) = intercalate " + " $ splitOn "," (init $ tail $ show ps)
 
 instance (Num a, Eq a) => Num (PowS' a) where
-   fromInteger c = fromInteger c
-   
-   negate (PowS' fs) = PowS' (map negate fs)
+  fromInteger c = PowS' [fromInteger c]
+  
+  negate (PowS' fs) = PowS' (map negate fs)
+  
+  PowS' (f:ft) + PowS' (g:gt) = PowS' (f+g:t) where PowS' t = PowS' ft + PowS' gt
+  fs + PowS' [] = fs
+  PowS' [] + gs = gs
 
-   PowS' (f:ft) + PowS' (g:gt) = PowS' (f+g:t) where PowS' t = PowS' ft + PowS' gt
-   fs + PowS' [] = fs
-   PowS' [] + gs = gs
-
-   PowS' (0:ft) * gs = PowS' (0:t) where PowS' t = PowS' ft * gs   -- caveat: 0*diverge = 0
-   PowS' fs * PowS' (0:gt) = PowS' (0:t) where PowS' t =  PowS' fs * PowS' gt
-   PowS' (f:ft) * PowS' gs@(g:gt) = PowS' (f*g:t) where PowS' t =  PowS' ft * PowS' gs + PowS' [f] * PowS' gt
-   _ * _ = PowS' []
+  PowS' (0:ft) * gs = PowS' (0:t) where PowS' t = PowS' ft * gs   -- caveat: 0*diverge = 0
+  PowS' fs * PowS' (0:gt) = PowS' (0:t) where PowS' t =  PowS' fs * PowS' gt
+  PowS' (f:ft) * PowS' gs@(g:gt) = PowS' (f*g:t) where PowS' t =  PowS' ft * PowS' gs + PowS' [f] * PowS' gt
+  _ * _ = PowS' []
 
 instance (Fractional a, Eq a) => Fractional (PowS' a) where
-   fromRational c = fromRational c
-
-   PowS' (0:ft) / PowS' gs@(0:gt) = PowS' ft / PowS' gt
-   PowS' (0:ft) / gs = PowS' (0:t) where PowS' t = PowS' ft / gs
-   PowS' (f:ft) / PowS' gs@(g:gt) = PowS' (f/g:t) where PowS' t = PowS' ft - PowS' [f/g] * PowS' gt / PowS' gs
-   PowS' [] / PowS' (0:gt) = PowS' [] / PowS' gt
-   PowS' [] / PowS' (g:gt) = PowS' []
-   _ / _ = error "improper power series division"
+  fromRational c = PowS' [fromRational c]
+  
+  PowS' (0:ft) / PowS' gs@(0:gt) = PowS' ft / PowS' gt
+  PowS' (0:ft) / gs = PowS' (0:t) where PowS' t = PowS' ft / gs
+  PowS' (f:ft) / PowS' gs@(g:gt) = PowS' (f/g:t) where PowS' t = PowS' ft - PowS' [f/g] * PowS' gt / PowS' gs
+  PowS' [] / PowS' (0:gt) = PowS' [] / PowS' gt
+  PowS' [] / PowS' (g:gt) = PowS' []
+  _ / _ = error "improper power series division"
 
 (#) :: (Eq a, Num a) => PowS' a -> PowS' a -> PowS' a
 PowS' (f:ft) # gs@(PowS' (0:gt)) = PowS' (f : gt) * PowS' ft # gs
@@ -70,6 +71,9 @@ diff (PowS' (_:ft)) = PowS' $ zipWith (*) ft (map fromInteger [1..])
 
 tans :: PowS
 tans = revert $ int 1 / PowS' [1,0,1]
+
+exps :: PowS
+exps = 1 + int exps
 
 sins :: PowS
 sins = int coss
