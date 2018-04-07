d3c6fd2065fd7a7d1ca552247bee502790c50216
Sophia Gold <sophiagoldnyc@gmail.com>
2018-04-06 23:23:08 -0400

    s/decl/expr + record accessor for newtype

--- src/PowerSerious.hs
+++ src/PowerSerious.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MonoLocalBinds             #-}
 {-# LANGUAGE RankNTypes                 #-}
 {-# OPTIONS_GHC -Wno-missing-methods    #-}
 
@@ -12,65 +11,74 @@ import Data.List.Split
 import Data.Ratio
 
 default (Integer, Rational, Double)
-infixr 9 #
 
-newtype PowS' a = PowS' [a] deriving Eq
--- newtype PowS' a = PowS' { fromPowS' :: [a] } deriving Eq
+newtype PowS' a = PowS' { fromPowS :: [a] } deriving Eq
 
-type PowS = forall a . (Eq a, Fractional a, Num (PowS' a), Fractional (PowS' a)) => PowS' a
-type PowS2 = forall a . (Eq a, Fractional a, Num (PowS' a), Fractional (PowS' a)) => PowS' (PowS' a)
-  
-instance {-# OVERLAPPING #-} Show a => Show (PowS' (Ratio a)) where 
-  show (PowS' ps) = intercalate " + " $ showPS ps where
+type PowS = forall a . (Eq a, Fractional a) => PowS' a
+type PowS2 = forall a . (Eq a, Fractional a) => PowS' (PowS' a)
+
+instance {-# OVERLAPPING #-} Show (PowS' Rational) where
+  show ps = intercalate " + " $ showPS $ fromPowS ps where
     showPS ps = map (\p -> (show $ numerator p) ++ "/" ++ (show $ denominator p)) ps
 
-instance Show a => Show (PowS' a) where 
-  show (PowS' ps) = intercalate " + " $ splitOn "," (init $ tail $ show ps)
+instance {-# INCOHERENT #-} Show a => Show (PowS' a) where
+  show ps = intercalate " + " $ splitOn "," (init $ tail $ show $ fromPowS ps)
 
+instance {-# OVERLAPPING #-} Show a => Show (PowS' (PowS' a)) where
+  show ps = intercalate " + " $ splitOn "," $ init $ tail $
+            intercalate "\n" $ splitOn "],[" (init $ tail $ show $ (fromPowS <$> fromPowS ps))
+  
 instance (Num a, Eq a) => Num (PowS' a) where
   fromInteger c = PowS' [fromInteger c]
   
-  negate (PowS' fs) = PowS' (map negate fs)
-  
-  PowS' (f:ft) + PowS' (g:gt) = PowS' (f+g:t) where PowS' t = PowS' ft + PowS' gt
-  fs + PowS' [] = fs
-  PowS' [] + gs = gs
+  negate fs = PowS' (map negate $ fromPowS fs)
 
-  PowS' (0:ft) * gs = PowS' (0:t) where PowS' t = PowS' ft * gs   -- caveat: 0*diverge = 0
-  PowS' fs * PowS' (0:gt) = PowS' (0:t) where PowS' t =  PowS' fs * PowS' gt
-  PowS' (f:ft) * PowS' gs@(g:gt) = PowS' (f*g:t) where PowS' t =  PowS' ft * PowS' gs + PowS' [f] * PowS' gt
-  _ * _ = PowS' []
+  fs + gs = case (fromPowS fs, fromPowS gs) of
+    (f:ft, g:gt) -> PowS' (f+g:t) where  PowS' t = PowS' ft + PowS' gt
+    (fs, [])     -> PowS' fs
+    ([], gs)     -> PowS' gs
 
+  fs * gs = case (fromPowS fs, fromPowS gs) of
+    (0:ft, gs)   -> PowS' (0:t) where PowS' t = PowS' ft * PowS' gs   -- caveat: 0*diverge = 0
+    (fs, 0:gt)   -> PowS' (0:t) where PowS' t =  PowS' fs * PowS' gt
+    (f:ft, g:gt) -> PowS' (f*g:t) where PowS' t =  PowS' ft * gs + PowS' [f] * PowS' gt
+    (_,_)        ->  PowS' []
+    
 instance (Fractional a, Eq a) => Fractional (PowS' a) where
   fromRational c = PowS' [fromRational c]
+
+  fs / gs = case (fromPowS fs, fromPowS gs) of
+    (0:ft, 0:gt) -> PowS' ft / PowS' gt
+    (0:ft, gs)   -> PowS' (0:t) where PowS' t = PowS' ft / PowS' gs
+    (f:ft, g:gt) -> PowS' (f/g:t) where PowS' t = PowS' ft - PowS' [f/g] * PowS' gt / gs
+    ([], 0:gt)   -> PowS' [] / PowS' gt
+    ([], g:gt)   -> PowS' []
+    (_,_)        -> error "improper power series division"
   
-  PowS' (0:ft) / PowS' gs@(0:gt) = PowS' ft / PowS' gt
-  PowS' (0:ft) / gs = PowS' (0:t) where PowS' t = PowS' ft / gs
-  PowS' (f:ft) / PowS' gs@(g:gt) = PowS' (f/g:t) where PowS' t = PowS' ft - PowS' [f/g] * PowS' gt / PowS' gs
-  PowS' [] / PowS' (0:gt) = PowS' [] / PowS' gt
-  PowS' [] / PowS' (g:gt) = PowS' []
-  _ / _ = error "improper power series division"
-
+infixr 9 #
 (#) :: (Eq a, Num a) => PowS' a -> PowS' a -> PowS' a
-PowS' (f:ft) # gs@(PowS' (0:gt)) = PowS' (f : gt) * PowS' ft # gs
-PowS' (f:ft) # gs@(PowS' (g:gt)) = PowS' [f] + gs * PowS' ft # gs -- ft must be polynomial
-PowS' [] # _ = PowS' []
-PowS' (f:_) # PowS' [] = PowS' [f]
+fs # gs = case (fromPowS fs, fromPowS gs) of
+  (f:ft, gs@(0:gt)) -> PowS' (f:gt) * PowS' ft # PowS' gs
+  (f:ft, gs@(g:gt)) -> PowS' [f] + PowS' gs * PowS' ft # PowS' gs  -- ft must be polynomial
+  ([], _)           -> PowS' []
+  (f:_, [])         -> PowS' [f]
 
-revert :: (Eq a, Num a, Fractional a) => PowS' a -> PowS' a
-revert (PowS' (_:0:_)) = error "revert f where f'(0)==0"
-revert (PowS' (0:ft)) = rs where rs = PowS' ([0, 1]) / PowS' ft # rs
-revert (PowS' [f,f']) = PowS' [-f/f',1/f']
-revert _ = error "revert f where f(0)/=0"
+revert :: (Eq a, Fractional a) => PowS' a -> PowS' a
+revert fs = case fromPowS fs of
+  (_:0:_) -> error "revert f where f'(0)==0"
+  -- (0:ft)  -> PowS' rs where rs =  0 : fromPowS (1 / PowS' ft # PowS' rs)
+  (0:ft)  -> PowS' (0:rs) where PowS' rs = 1 / PowS' ft # PowS' rs
+  [f,f']  -> PowS' [-f/f',1/f']
+  _       -> error "revert f where f(0)/=0"
 
-int :: (Fractional a, Num (PowS' a), Fractional (PowS' a)) => PowS' a -> PowS' a
-int (PowS' fs) = PowS' (0 : zipWith (/) fs (map fromInteger [1..]))
+int :: (Fractional a) => PowS' a -> PowS' a
+int fs = PowS' (0 : zipWith (/) (fromPowS fs) (map fromInteger [1..]))
 
-diff :: (Num a, Num (PowS' a), Fractional (PowS' a)) => PowS' a -> PowS' a
-diff (PowS' (_:ft)) = PowS' $ zipWith (*) ft (map fromInteger [1..])
+diff :: (Num a) => PowS' a -> PowS' a
+diff fs = PowS' $ zipWith (*) (tail $ fromPowS fs) (map fromInteger [1..])
 
-tans :: PowS
-tans = revert $ int 1 / PowS' [1,0,1]
+takeS :: Int -> PowS' a -> PowS' a
+takeS i s = PowS' (take i $ fromPowS s)
 
 exps :: PowS
 exps = 1 + int exps
@@ -81,8 +89,11 @@ sins = int coss
 coss :: PowS
 coss = 1 - int sins
 
+tans :: PowS
+tans = revert $ int (1 / PowS' [1,0,1])
+
+tans' :: PowS
+tans' = sins / coss
+
 pascal :: PowS2
 pascal = 1 / PowS' [1, - PowS' [1,1]]
-
-takeS :: Int -> PowS -> PowS
-takeS i (PowS' s) = PowS' (take i s)
