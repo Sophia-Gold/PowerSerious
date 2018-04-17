74222db596da4437147ceedf885747441b8d620b
Sophia Gold <sophiagoldnyc@gmail.com>
2018-04-17 05:31:43 -0400

    ComonadApply, wu = liftW2, and <:>

--- src/PowerSerious.hs
+++ src/PowerSerious.hs
@@ -19,8 +19,15 @@ instance Comonad PowS' where
   extract (PowS' [a]) = a
   duplicate a = PowS' [a]
 
-wu :: [a] -> (PowS' a -> PowS' a -> PowS' a) -> [a] -> [a]
-wu fs op gs = fromPowS $ op (PowS' fs) (PowS' gs)
+instance ComonadApply PowS' where 
+  f <@> a = extract f <$> a
+
+infixr 5 <:>
+(<:>) :: a -> PowS' a -> PowS' a
+f <:> fs = PowS' (f : (fromPowS fs))
+
+wu :: [a] -> (a -> a -> a) -> [a] -> PowS' a
+wu fs op gs = liftW2 op (PowS' fs) (PowS' gs)
 
 type PowS = PowS' Rational
 type PowS2 = PowS' (PowS' Rational)
@@ -39,44 +46,44 @@ instance {-# OVERLAPPING #-} Show a => Show (PowS' (PowS' a)) where
 
 instance (Num a, Eq a) => Num (PowS' a) where
   fromInteger c = PowS' [fromInteger c]
-  
-  negate fs = PowS' (map negate $ fromPowS fs)
 
-  fs + gs = PowS' $ case (fromPowS fs, fromPowS gs) of
-    (f:ft, g:gt) -> f+g : wu ft (+) gt
-    (fs, [])     -> fs
-    ([], gs)     -> gs
+  negate fs = negate <$> fs
 
-  fs * gs = PowS' $ case (fromPowS fs, fromPowS gs) of
-    (0:ft, gs)   -> 0 : wu ft (*) gs   -- caveat: 0*diverge = 0
-    (fs, 0:gt)   -> 0 : wu fs (*) gt 
-    (f:ft, gs@(g:gt)) -> f*g : wu (wu ft (*) gs) (+) (wu [f] (*) gt)
-    (_,_)        -> []
+  fs + gs = case (fromPowS fs, fromPowS gs) of 
+    (f:ft, g:gt) -> f+g <:> wu ft (+) gt
+    (fs, [])     -> PowS' fs
+    ([], gs)     -> PowS' gs
+
+  fs * gs = case (fromPowS fs, fromPowS gs) of
+    (0:ft, gs)        -> 0 <:> wu ft (*) gs   -- caveat: 0*diverge = 0
+    (fs, 0:gt)        -> 0 <:> wu fs (*) gt 
+    (f:ft, gs@(g:gt)) -> f*g <:> wu ft (*) gs + wu [f] (*) gt
+    (_,_)             -> PowS' []
     
 instance (Fractional a, Eq a) => Fractional (PowS' a) where 
   fromRational c = PowS' [fromRational c]
 
-  fs / gs = PowS' $ case (fromPowS fs, fromPowS gs) of
+  fs / gs = case (fromPowS fs, fromPowS gs) of
     (0:ft, 0:gt) -> wu ft (/) gt
-    (0:ft, gs)   -> 0 : wu ft (/) gs 
-    (f:ft, gs@(g:gt)) -> f/g : wu (wu ft (-) $ wu [f/g] (*) gt) (/) gs
+    (0:ft, gs)   -> 0 <:> wu ft (/) gs 
+    (f:ft, g:gt) -> f/g <:> (PowS' ft - wu [f/g] (*) gt) / gs
     ([], 0:gt)   -> wu [] (/) gt
-    ([], g:gt)   -> []
+    ([], g:gt)   -> PowS' []
     (_,_)        -> error "improper power series division"
   
 infixr 9 #
-(#) :: (Eq a, Num a) => PowS' a -> PowS' a -> PowS' a 
-fs # gs = PowS' $ case (fromPowS fs, fromPowS gs) of 
-  (f:ft, gs@(0:gt)) -> f : (wu gt (*) $ wu ft (#) gs) 
-  (f:ft, gs@(g:gt)) -> wu [f] (+) $ wu gs (*) $ wu ft (#) gs  -- ft must be polynomial
-  ([], _)      -> []
-  (f:_, [])    -> [f]
+(#) :: (Eq a, Num a) => PowS' a -> PowS' a -> PowS' a
+fs # gs = case (fromPowS fs, fromPowS gs) of 
+  (f:ft, 0:gt) -> f <:> PowS' gt * PowS' ft # gs
+  (f:ft, g:gt) -> PowS' [f] + gs * PowS' ft # gs  -- ft must be polynomial
+  ([], _)           -> PowS' []
+  (f:_, [])         -> PowS' [f]
 
 revert :: (Eq a, Fractional a) => PowS' a -> PowS' a 
-revert fs = PowS' $ case fromPowS fs of
+revert fs = case fromPowS fs of
   (_:0:_) -> error "revert f where f'(0)==0" 
-  (0:ft)  -> rs where rs = 0 : (wu [1] (/) $ wu ft (#) rs)
-  [f,f']  -> [-f/f',1/f']
+  (0:ft)  -> rs where rs = 0 <:> PowS' [1] / PowS' ft # rs
+  [f,f']  -> PowS' [-f/f',1/f']
   _       -> error "revert f where f(0)/=0"
 
 int :: (Fractional a, Enum a) => PowS' a -> PowS' a 
