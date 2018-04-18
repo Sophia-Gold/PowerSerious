e2b0ce8d1e22945cdae10e836eaaf94d8b27a6cd
Sophia Gold <sophiagoldnyc@gmail.com>
2018-04-18 09:55:26 -0400

    supernecklace example

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import PowerSerious
+import Control.Comonad
+import Data.List
 
 exps :: PowS
 exps = 1 + int exps
@@ -15,7 +17,15 @@ tans :: PowS
 tans = revert $ int (1 / PowS' [1,0,1])
 
 pascal :: PowS2
-pascal = 1 / PowS' [1, - PowS' [1,1]]
+pascal = 1 / PowS' [1, PowS' [1,1]]
+
+supernecklace' :: Int -> Int -> Int
+supernecklace' degree size = length . last . take degree $ iterate
+                             (\s -> enumFromTo 1 . length . tail $ subsequences s)
+                             (enumFromTo 1 size)
+
+-- supernecklace :: Int -> PowS' Int
+-- supernecklace n = iterate (liftW2 $ tail . subsequences) (PowS' [1..n])
 
 main :: IO ()
 main = print $ takeS 10 pascal
