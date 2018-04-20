98f3ccfb63a7a5921bfd6ec57721571d5fef2fb4
Sophia Gold <sophiagoldnyc@gmail.com>
2018-04-20 02:32:09 -0400

    merge comonad branch

--- app/Main.hs
+++ app/Main.hs
@@ -1,31 +1,32 @@
 module Main where
 
 import PowerSerious
-import Control.Comonad
 import Data.List
 
-exps :: PowS
+exps :: PowS Rational
 exps = 1 + int exps
 
-sins :: PowS
+sins :: PowS Rational
 sins = int coss
 
-coss :: PowS
+coss :: PowS Rational
 coss = 1 - int sins
 
-tans :: PowS
-tans = revert $ int (1 / PowS' [1,0,1])
+tans :: PowS Rational
+tans = revert $ int (1 / PowS [1,0,1])
 
-pascal :: PowS2
-pascal = 1 / PowS' [1, PowS' [1,1]]
+pascal :: PowS (PowS Rational)
+pascal = 1 / PowS [1, - PowS [1,1]]
 
 supernecklace' :: Int -> Int -> Int
 supernecklace' degree size = length . last . take degree $ iterate
                              (\s -> enumFromTo 1 . length . tail $ subsequences s)
                              (enumFromTo 1 size)
 
--- supernecklace :: Int -> PowS' Int
--- supernecklace n = iterate (liftW2 $ tail . subsequences) (PowS' [1..n])
+-- degree 3
+-- supernecklace :: Int -> PowS (PowS (PowS (Int)))
+supernecklace :: Int -> PowS [[Int]]
+supernecklace n = (tail . subsequences) <$> (concat . tail . subsequences) <$> PowS [[1..n]]
 
 main :: IO ()
 main = print $ takeS 10 pascal
--- src/PowerSerious.hs
+++ src/PowerSerious.hs
@@ -12,63 +12,57 @@ import Data.List.Split
 import Data.Maybe
 import Data.Ratio
 
-newtype PowS' a = PowS' { fromPowS :: [a] } deriving Eq
+newtype PowS a = PowS { fromPowS :: [a] } deriving Eq
 
-instance Functor PowS' where
-  fmap f (PowS' a) = PowS' (fmap f a)
+instance Functor PowS where
+  fmap f (PowS a) = PowS (fmap f a)
 
-instance Comonad PowS' where
-  -- duplicate p ps = p <:> ps 
-  -- extract ps = PowS' (tail $ fromPowS ps) --infix operator for tail
-  duplicate a = PowS' [a]
-  extract (PowS' [a]) = a  
-
-instance ComonadApply PowS' where
-  f <@> a = extract f <$> a
+-- instance Comonad PowS where
+--   -- duplicate p ps = p <:> ps
+--   -- extract ps = PowS (tail $ fromPowS ps) --infix operator for tail 
+--   -- extract (PowS [a]) = a
+--   -- duplicate a = PowS [a]
+  
+-- instance ComonadApply PowS where
+--   f <@> a = extract f <$> a
 
 infixr 5 <:>
-(<:>) :: a -> PowS' a -> PowS' a
-f <:> fs = PowS' (f : (fromPowS fs))
+(<:>) :: a -> PowS a -> PowS a
+f <:> fs = PowS (f : (fromPowS fs))
 
-headPS :: PowS' a -> Maybe a
-headPS ps = listToMaybe $ fromPowS ps
-
-tailPS :: PowS' a -> Maybe (PowS' a)
-tailPS ps = case fromPowS ps of
+splitPS :: PowS a -> (Maybe a, Maybe (PowS a))
+splitPS = (,) <$> headPS <*> tailPS where
+  headPS ps = listToMaybe $ fromPowS ps
+  tailPS ps = case fromPowS ps of
               [] -> Nothing
-              x -> Just (PowS' (tail x))
+              x -> Just (PowS (tail x))
 
-splitPS :: PowS' a -> (Maybe a, Maybe (PowS' a))
-splitPS = (,) <$> headPS <*> tailPS
-
--- splitPS :: PowS' a -> (a, PowS' a)
--- splitPS ps = (head u, PowS' (tail u)) where u = fromPowS ps
+-- splitPS :: PowS a -> (a, PowS a)
+-- splitPS ps = (head u, PowS (tail u)) where u = fromPowS ps
   
 pattern f :<>: ft <- (Just f, Just ft)
-pattern (:|:)     <- (Nothing, Nothing)
+pattern (:|:)     <- (Nothing, _)
 
--- pattern f :<>: ft <- (f, ft)
-
-wu :: [a] -> (a -> a -> a) -> [a] -> PowS' a
-wu fs op gs = liftW2 op (PowS' fs) (PowS' gs)
-
-type PowS = PowS' Rational
-type PowS2 = PowS' (PowS' Rational)
-
-instance {-# OVERLAPPING #-} Show (PowS' Rational) where
+instance {-# OVERLAPPING #-} Show (PowS Rational) where
   show ps = intercalate " + " $ showPS $ fromPowS ps where
     showPS ps = map (\p -> if denominator p == 1
                            then show $ numerator p
                            else (show $ numerator p) ++ "/" ++ (show $ denominator p))
                 ps
+
+instance {-# OVERLAPPING #-} Show a => Show (PowS [a]) where
+  show ps = substitute " + " "," $ substitute "" "]" $ substitute "" "[" $ substitute "\n" "],[" $
+    init $ tail $ show $ fromPowS ps where
+    substitute x y z = intercalate x $ splitOn y $ z
                 
-instance {-# OVERLAPPING #-} Show a => Show (PowS' (PowS' a)) where
+instance {-# OVERLAPPING #-} Show a => Show (PowS (PowS a)) where
   show ps = substitute " + " "," $ substitute "\n" "],[" $ init $ tail $
             substitute "" " % 1" $ init $ tail $ show $ (fromPowS <$> fromPowS ps) where
     substitute x y z = intercalate x $ splitOn y $ z
 
-instance (Num a, Eq a) => Num (PowS' a) where
-  fromInteger c = PowS' [fromInteger c]
+instance (Num a, Eq a) => Num (PowS a) where
+  fromInteger c = PowS [fromInteger c]
+  -- fromInteger c = duplicate $ fromInteger c
 
   negate fs = negate <$> fs
 
@@ -77,53 +71,49 @@ instance (Num a, Eq a) => Num (PowS' a) where
     (_, (:|:))         -> fs
     ((:|:), _)         -> gs
 
-  -- fs + gs = case (fromPowS fs, fromPowS gs) of 
-  --   (f:ft, g:gt) -> f+g <:> wu ft (+) gt
-  --   (fs, [])     -> PowS' fs
-  --   ([], gs)     -> PowS' gs
+  fs * gs = case (splitPS fs, splitPS gs) of
+    (0:<>:ft, _)       -> 0 <:> ft * gs   -- caveat: 0*diverge = 0
+    (_, 0:<>:gt)       -> 0 <:> fs * gt 
+    (f:<>:ft, g:<>:gt) -> f*g <:> ft * gs + PowS [f] * gt
+    (_,_)              -> PowS []
 
-  fs * gs = case (fromPowS fs, fromPowS gs) of
-    (0:ft, gs)        -> 0 <:> wu ft (*) gs   -- caveat: 0*diverge = 0
-    (fs, 0:gt)        -> 0 <:> wu fs (*) gt 
-    (f:ft, gs@(g:gt)) -> f*g <:> wu ft (*) gs + wu [f] (*) gt
-    (_,_)             -> PowS' []
-    
-instance (Fractional a, Eq a) => Fractional (PowS' a) where 
-  fromRational c = PowS' [fromRational c]
+instance (Fractional a, Eq a) => Fractional (PowS a) where 
+  fromRational c = PowS [fromRational c]
+  -- fromRational c = duplicate $ fromRational c
 
-  fs / gs = case (fromPowS fs, fromPowS gs) of
-    (0:ft, 0:gt) -> wu ft (/) gt
-    (0:ft, gs)   -> 0 <:> wu ft (/) gs 
-    (f:ft, g:gt) -> f/g <:> (PowS' ft - wu [f/g] (*) gt) / gs
-    ([], 0:gt)   -> wu [] (/) gt
-    ([], g:gt)   -> PowS' []
-    (_,_)        -> error "improper power series division"
+  fs / gs = case (splitPS fs, splitPS gs) of
+    (0:<>:ft, 0:<>:gt) -> ft / gt
+    (0:<>:ft, _)       -> 0 <:> ft / gs 
+    (f:<>:ft, g:<>:gt) -> f/g <:> (ft - PowS [f/g] * gt) / gs
+    ((:|:), 0:<>:gt)   -> PowS [] / gt
+    ((:|:), g:<>:gt)   -> PowS []
+    (_,_)              -> error "improper power series division"
   
 infixr 9 #
-(#) :: (Eq a, Num a) => PowS' a -> PowS' a -> PowS' a
-fs # gs = case (fromPowS fs, fromPowS gs) of 
-  (f:ft, 0:gt) -> f <:> PowS' gt * PowS' ft # gs
-  (f:ft, g:gt) -> PowS' [f] + gs * PowS' ft # gs  -- ft must be polynomial
-  ([], _)           -> PowS' []
-  (f:_, [])         -> PowS' [f]
+(#) :: (Eq a, Num a) => PowS a -> PowS a -> PowS a
+fs # gs = case (splitPS fs, splitPS gs) of 
+  (f:<>:ft, 0:<>:gt) -> f <:> gt * ft # gs
+  (f:<>:ft, g:<>:gt) -> PowS [f] + gs * ft # gs  -- ft must be polynomial
+  ((:|:), _)         -> PowS []
+  (f:<>:_, (:|:))    -> PowS [f]
 
-revert :: (Eq a, Fractional a) => PowS' a -> PowS' a 
+revert :: (Eq a, Fractional a) => PowS a -> PowS a 
 revert fs = case fromPowS fs of
   (_:0:_) -> error "revert f where f'(0)==0" 
-  (0:ft)  -> rs where rs = 0 <:> PowS' [1] / PowS' ft # rs
-  [f,f']  -> PowS' [-f/f',1/f']
+  (0:ft)  -> rs where rs = 0 <:> PowS [1] / PowS ft # rs
+  [f,f']  -> PowS [-f/f',1/f']
   _       -> error "revert f where f(0)/=0"
 
-int :: (Fractional a, Enum a) => PowS' a -> PowS' a 
-int fs = PowS' (0 : zipWith (/) (fromPowS fs) [1..])
+int :: (Fractional a, Enum a) => PowS a -> PowS a 
+int fs = PowS (0 : zipWith (/) (fromPowS fs) [1..])
 
-diff :: (Num a, Enum a) => PowS' a -> PowS' a
-diff fs = PowS' $ zipWith (*) (tail $ fromPowS fs) [1..]
+diff :: (Num a, Enum a) => PowS a -> PowS a
+diff fs = PowS $ zipWith (*) (tail $ fromPowS fs) [1..]
 
 -- replaces `take` from prelude
-takeS :: Int -> PowS' a -> PowS' a
-takeS i s = PowS' (take i $ fromPowS s)
+takeS :: Int -> PowS a -> PowS a
+takeS i s = PowS (take i $ fromPowS s)
 
 -- like `takeS` but returns a vanilla list
-takeList :: Int -> PowS' a -> [a]
+takeList :: Int -> PowS a -> [a]
 takeList i s = take i $ fromPowS s
